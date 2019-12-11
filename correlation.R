2db3f88c2c96a53c478e5e079900bc32b0bbb886
MughilM <mughilan.muthupari@gmail.com>
2019-12-10 23:21:32 -0500

    Added initial report! Pure plotly for correlation.
    Huzzah the report has been started! The data source and the first
    analysis has been finished. The correlation plot has been changed to
    pure plotly.

--- correlation.R
+++ correlation.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(plotly)
 library(reshape2)
+library(colorspace)
 wdi <- read.table('./WDI-tidied.csv', sep = ',', header = TRUE, check.names = FALSE)
 happy <- read.table('./happy.csv', sep = ',', header = TRUE, check.names = FALSE)
 
@@ -15,13 +16,29 @@ cormat = cor(x = mergedData[,3:128], y = mergedData[,129:134], use = "complete.o
 
 # Now build the plot...
 meltedCor = melt(cormat)
-ggCor <- ggplot(meltedCor, aes(x = Var1, y = Var2, fill = value)) + 
-  geom_tile() + 
-  theme(axis.text.x = element_blank(), 
-        axis.ticks.x = element_blank(),
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank()) + 
-  scale_fill_continuous_diverging()
-# Convert to plotly
-ggplotly(ggCor)
+colnames(meltedCor) <- c("WDI", "Happiness", "Correlation")
+# ggCor <- ggplot(meltedCor, aes(x = WDI, y = Happiness, fill = Correlation)) + 
+#   geom_tile() + 
+#   theme(axis.text.x = element_blank(), 
+#         axis.ticks.x = element_blank(),
+#         axis.title.x = element_blank(),
+#         axis.title.y = element_blank()) + 
+#   scale_fill_continuous_diverging() +
+#   labs(title = "Correlation of WDI Indicators vs. Happiness Indicators")
+# # Convert to plotly
+# t <- list(
+#   family = "'Comfortaa', cursive"
+# )
+# ggplotly(ggCor) %>% layout(font=t)
+
+p <- plot_ly(x = row.names(cormat), 
+             y = colnames(cormat), 
+             z = t(as.matrix(cormat)), 
+             type = "heatmap") %>%
+  layout(
+    font = list(family = "'Montserrat', sans-serif"),
+    title = "Correlation of WB Indicators vs. Happiness Indicators in 2018",
+    xaxis = list(showticklabels = FALSE, showgrid = FALSE)
+  )
+p
 
